7d1afed490b995e92910ffb126a593f825bd809c
jessicasalbert <jessicarsalbert@gmail.com>
2020-11-24 16:09:04 -0500

    routing working hackily

--- app/serializers/patient_serializer.rb
+++ app/serializers/patient_serializer.rb
@@ -1,7 +1,11 @@
 class PatientSerializer < ApplicationSerializer
-    attributes :id, :email, :name, :patient_meds
+    attributes :id, :email, :name, :patient_meds, :doctor
 
     def patient_meds
         ActiveModel::SerializableResource.new(object.patient_meds,  each_serializer: PatientMedSerializer)
+    end
+
+    def doctor
+        {name: self.object.doctor.name, email: self.object.doctor.email, id: self.object.doctor.id}
     end
 end
